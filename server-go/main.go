4105700e2a9033748ffa54c4b179b62f357b52b5
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-06-14 14:08:44 +0530

    Update main.go

--- server-go/main.go
+++ server-go/main.go
@@ -9,7 +9,7 @@ func main() {
 
  http.HandleFunc("/api/v1/ping", ping)
  fmt.Println("Server Application running.")
- http.ListenAndServe(":8082", nil)
+ http.ListenAndServe(":8080", nil)
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
